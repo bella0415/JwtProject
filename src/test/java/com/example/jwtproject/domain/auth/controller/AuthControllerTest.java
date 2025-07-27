b4c2f61bdbd5a4ae8286be031bb8f48b84800037
bella <yiseuljeong0415@gmail.com>
2025-07-27 20:12:04 +0900

    fix : 버전에을 낮췄기에 mokitobean -> mockbean 으로 수정

--- src/test/java/com/example/jwtproject/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/com/example/jwtproject/domain/auth/controller/AuthControllerTest.java
@@ -19,10 +19,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -40,7 +39,7 @@ class AuthControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@MockitoBean
+	@MockBean
 	private AuthService authService;
 
 	@Nested
